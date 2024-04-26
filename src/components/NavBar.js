f3e5a0e57bdcea3445c330da18319012a6b5ec52
Mahdi209 <dr.arcadea@gmail.com>
2024-04-26 19:44:49 +0300

    edit name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,7 +19,7 @@ export default function NavBar() {
   return (
     <>
       <nav className="navBar">
-        <div>Portfolio</div>
+        <div>Mohammed Mahdi</div>
         <ul className="contact">
           <li>
             <Link to="home" duration={500}>
